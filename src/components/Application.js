77cd5b891eb9374a6c9d6a9cd33c79d26d1f642f
Avijit Choudhury <avijit.choudhury24@gmail.com>
2019-10-29 17:32:27 -0400

    The schedule

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import "components/Application.scss";
 import DayList from "components/DayList";
+import Appointment from "components/Appointment";
+import classnames from "classnames";
 
 const days = [
   {
@@ -20,8 +22,72 @@ const days = [
   },
 ];
 
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 3,
+    time: '2pm',
+    interview: {
+      student: "Kawhi Leonard",
+      interviewer: {
+        id: 2,
+        name: "Tori Malcolm",
+        avatar: "https://i.imgur.com/Nmx0Qxo.png",
+      }
+    }
+  },
+    {
+    id: 4,
+    time: '3pm',
+    interview: {
+      student: "Lebron James",
+      interviewer: {
+        id: 3,
+        name: "Mildred Nazir",
+        avatar: "https://i.imgur.com/T2WwVfS.png" ,
+      }
+    }
+  },
+   {
+    id: 5,
+    time: '4pm',
+    interview: {
+      student: "Giannis Antetekoumpo",
+      interviewer: {
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg" ,
+      }
+    }
+  }
+];
+
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
+  const makeAppt = appointments.map( appointment => {
+  return (
+    <Appointment
+      key={appointment.id}
+      id={appointment.id}
+      time={appointment.time}
+      interview={appointment.interview} />
+  )
+  })
   return (
     <main className="layout">
       <section className="sidebar">
@@ -44,8 +110,9 @@ export default function Application(props) {
           alt="Lighthouse Labs"
         />
       </section>
+      
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+      return {makeAppt}
       </section>
     </main>
   );
