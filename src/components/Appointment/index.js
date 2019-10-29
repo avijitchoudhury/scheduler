aabd3bf69ac551057b97222645a2c2bd0860ba24
Avijit Choudhury <avijit.choudhury24@gmail.com>
2019-10-29 12:35:08 -0400

    confirm/show

---
+++ src/components/Appointment/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "components/Appointment/styles.scss";
+import { storiesOf } from "@storybook/react";
+
+export default function Appointment (props) {
+  return (
+    <article className="appointment"></article>
+  )
+}
+
